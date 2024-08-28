e807c1213658b325bbd3b6be9c65a544da134922
limengxun <limengxun45@outlook.com>
2024-08-28 16:32:51 +0800

    remove v1

--- bindings/setup.py
+++ bindings/setup.py
@@ -12,7 +12,7 @@ src_dir = __root_dir__ / "src"
 subprocess.check_call([ "python3", str(src_dir / "indexer_gen.py"), str(BOARD_SIZE) ])
 
 include_dir = __root_dir__ / "include"
-exclude_patterns = ["main.cpp", "benchmark.cpp", "*_test.cpp", "solver_v1*", "indexer*.cpp"]
+exclude_patterns = ["main.cpp", "benchmark.cpp", "*_test.cpp", "indexer*.cpp"]
 cpp_files = [
     str(f) for f in src_dir.glob("*.cpp") 
     if not any(f.match(p) for p in exclude_patterns)
